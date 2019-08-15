a2636d9382fa606d28d091046a8d0f09b1b6f921
Moty Douek <49308633+MotyDouek@users.noreply.github.com>
2019-08-16 01:12:35 +0300

    Added shit tpon of stuff

--- ClientSide/millim-app/components/screens/DashboardMainScreen.js
+++ ClientSide/millim-app/components/screens/DashboardMainScreen.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import {View, Text, ScrollView, StyleSheet} from 'react-native';
+import {View, Text, ScrollView, StyleSheet, KeyboardAvoidingView } from 'react-native';
+import AppHeader from "../partials/AppHeader";
 import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import DefaultButton from "../partials/DefaultButton";
 
 
 export default class DashboardMainScreen extends React.PureComponent {
@@ -11,9 +14,19 @@ export default class DashboardMainScreen extends React.PureComponent {
 
     render() {
         return (
-            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                <Text>This is DashboardMainScreen</Text>
-            </View>)
+          <KeyboardAvoidingView  behavior={'height'} contentContainerStyle={{alignItems: 'center'}} style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <View style={styles.row}>
+                <Text style={styles.textStyle}>David Goldman</Text>
+                <DefaultButton buttonText={'Subscribe'} style={styles.button} textStyle={styles.buttonText} />
+            </View>
+            <View style={styles.content}>
+              <Text>Free member</Text>
+            </View>
+            <View style={styles.icon}>
+            <FontAwesome name="user-circle" size={136} color="#0372B9" />
+            </View>
+          </KeyboardAvoidingView>
+          )
 
     }
 
@@ -25,5 +38,36 @@ export default class DashboardMainScreen extends React.PureComponent {
 }
 
 
-const styles = StyleSheet.create({}
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  content: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    height: 28,
+    width: 95,
+    marginLeft: 10
+  },
+  buttonText: {
+    fontSize: 14,
+    color: 'white',
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    lineHeight: 21
+  },
+  textStyle: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginLeft: 100
+  },
+  icon: {
+    marginTop: 10
+  }
+}
 )
