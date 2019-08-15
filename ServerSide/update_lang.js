0a879b411b80edca39ea9fc876e5115aaf9b241d
itay <itayperry91@gmail.com>
2019-08-15 13:53:14 +0300

    test my permission

--- ServerSide/update_lang.js
+++ ServerSide/update_lang.js
@@ -1,4 +1,4 @@
-const mysql      = require('mysql');
+const mysql = require('mysql');
 
 // // The text to translate
 // const text = 'כלב';
@@ -7,40 +7,45 @@ const mysql      = require('mysql');
 // const target = 'en';
 
 let WordsConnection = mysql.createConnection({
-    host     : '192.168.1.35',
-    user     : 'eran',
-    password : 'EranShiffman1234!',
-    database : 'Words'
+  host: '192.168.1.35',
+  user: 'eran',
+  password: 'EranShiffman1234!',
+  database: 'Words'
 });
 
 async function main() {
-    WordsConnection.connect();
+  WordsConnection.connect();
 
-    // Imports the Google Cloud client library
-    const {Translate} = require('@google-cloud/translate');
+  // Imports the Google Cloud client library
+  const { Translate } = require('@google-cloud/translate');
 
-// // Instantiates a client
-    const translate = new Translate();
+  // // Instantiates a client
+  const translate = new Translate();
 
-    const [languages] = await translate.getLanguages().catch((err) =>{
-        throw err;
-    });
+  const [languages] = await translate.getLanguages().catch(err => {
+    throw err;
+  });
 
-    languages.forEach((lang)=>{
-        WordsConnection.query(`INSERT INTO languages (code,name) VALUES ('${lang.code}','${lang.name}')`, function (error, results, fields) {
-            if (error) throw error;
-            console.log(results);
-        });
-    });
+  languages.forEach(lang => {
+    WordsConnection.query(
+      `INSERT INTO languages (code,name) VALUES ('${lang.code}','${
+        lang.name
+      }')`,
+      function(error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+      }
+    );
+  });
 
-// // Translates some text into Russian
-//     translate.translate(text, target).then(async (translation)=>{
-//         console.log(`Text: ${text}`);
-//         console.log(`Translation: ${translation[0]}`);
-//         console.log(languages);
-//     });
+  // // Translates some text into Russian
+  //     translate.translate(text, target).then(async (translation)=>{
+  //         console.log(`Text: ${text}`);
+  //         console.log(`Translation: ${translation[0]}`);
+  //         console.log(languages);
+  //     });
 
-    WordsConnection.end();
+  WordsConnection.end();
 }
 
 main();
