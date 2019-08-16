3dc4071939fe34f3fa2660c33ccb6e24c6ff2ead
Ron Belkin <ron@storrsoft.com>
2019-08-16 06:01:22 +0300

    Added PlayScreen changes

--- ClientSide/millim-app/components/screens/PlayScreen.js
+++ ClientSide/millim-app/components/screens/PlayScreen.js
@@ -1,76 +1,114 @@
 import React, {useState} from 'react';
-import { StyleSheet, View, Text, KeyboardAvoidingView } from 'react-native';
-import { TabNavigator } from 'react-navigation';
-import { AntDesign } from '@expo/vector-icons';
-import { Feather } from '@expo/vector-icons';
+import {StyleSheet, View, Text, KeyboardAvoidingView} from 'react-native';
+import {TabNavigator} from 'react-navigation';
+import {AntDesign} from '@expo/vector-icons';
+import {Feather} from '@expo/vector-icons';
 import DefaultButton from "../partials/DefaultButton";
 
-class PlayScreen extends React.PureComponent{
 
-  constructor(props){
-    super(props);
-    this.state = {
-      
+let dummyData = [
+    {
+        answers: ['Cool', 'Sleepy', 'On Fire', 'Horrible'],
+        correctAnswer: 'Cool',
+        hebrewWord: 'סבבה'
     }
-  }
-
-  static navigationOptions = {
-        tabBarIcon: ({focused, tintColor}) => <AntDesign name="rocket1" size={30} color={tintColor} />
-      };
-
-  render(){
+];
 
 
+class PlayScreen extends React.PureComponent {
 
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedAnswer: '',
+            questionIndex: 0,
+            answerIndicator: ''
+        }
+    }
 
-        <Text style={fontSize=20}>Pick the best definition for this word.</Text>
-        <View style={styles.text}>
-          <Text> סבבה </Text>
-          <Feather name="help-circle" size={20} />
-        </View>
+    static navigationOptions = {
+        tabBarIcon: ({focused, tintColor}) => <AntDesign name="rocket1" size={30} color={tintColor}/>
+    };
 
-        <View style={styles.row}>
-          <DefaultButton buttonText={'Cool'} style={styles.button} textStyle={styles.buttonText} />
-          <DefaultButton buttonText={'Sleepy'} style={styles.button} textStyle={styles.buttonText} />
-        </View>
 
-        <View style={styles.row}>
-          <DefaultButton buttonText={'On fire'} style={styles.button} textStyle={styles.buttonText} />
-          <DefaultButton buttonText={'Subscribe'} style={styles.button} textStyle={styles.buttonText} />
-        </View>
+    selectAnswer = (answer) => {
+        this.setState({selectedAnswer: answer});
+    };
 
-    </View>
-  );
-};
+    checkAnswer = () => {
+        this.setState((prevState) => ({answerIndicator: prevState.selectedAnswer === dummyData[prevState.questionIndex].correctAnswer ? 'check-circle' : 'x-circle'}));
+    };
+
+
+    render() {
+        return (
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+
+                <Text style={{fontSize: 20}}>Pick the best definition for this word:</Text>
+
+                <Text style={{
+                    fontFamily: 'Roboto',
+                    fontSize: 24
+                }}> {dummyData[this.state.questionIndex].hebrewWord} </Text>
+
+
+                <View style={styles.row}>
+                    {
+                        dummyData[this.state.questionIndex].answers.map((answer) => (
+                            <DefaultButton key={answer} onPress={() => this.selectAnswer(answer)} buttonText={answer}
+                                           backgroundColor={this.state.selectedAnswer === answer ? '#0372B9' : '#817D7D'}
+                                           style={styles.button} textStyle={styles.buttonText}/>
+                        ))
+                    }
+
+                </View>
+
+                {
+                    this.state.selectedAnswer !== '' &&
+                    <View style={{width: '100%', paddingRight: 10}}>
+                        <DefaultButton onPress={this.checkAnswer} buttonText={'Check Answer'}
+                                       style={{alignSelf: 'flex-end', width: 150}}/>
+                    </View>
+                }
+
+                {
+                    this.state.answerIndicator !== '' &&
+
+                        <Feather color={this.state.answerIndicator === 'check-circle' ? 'green' : 'red'} name={this.state.answerIndicator} size={35}/>
+
+                }
+
+            </View>
+        );
+    };
 }
 
 
-
 const styles = StyleSheet.create({
-  row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
-    paddingTop: 10,
-    paddingHorizontal: 60
-  },
-  button: {
-    height: 28,
-    width: 95
-  },
-  text: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    width: '100%',
-    paddingTop: 10,
-    fontSize: 18
-  },
-  answerButton: {
-    height: 40,
-    width: 160
-  }
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+        paddingTop: 10,
+        paddingHorizontal: 60,
+        flexWrap: 'wrap',
+        marginBottom: 20
+    },
+    button: {
+        height: 28,
+        width: 95
+    },
+    text: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        width: '100%',
+        paddingTop: 10,
+        fontSize: 18
+    },
+    answerButton: {
+        height: 40,
+        width: 160
+    }
 });
 
 export default PlayScreen;
